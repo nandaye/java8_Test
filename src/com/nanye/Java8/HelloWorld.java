32e6ecfa012f717c19a6f173ec88f86b8b5821ee
nandaye <18281908275@163.com>
2016-02-15 23:27:46 +0800

    Initial commit to test git Add hello World!

---
+++ src/com/nanye/Java8/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.nanye.Java8;
+
+/**
+ * Created by nandaye on 2016/2/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        String my_name = "nandaye";
+        System.out.println("Yes ,this is a program to print hello world!");
+        System.out.println("my_name = " + my_name);
+    }
+}
