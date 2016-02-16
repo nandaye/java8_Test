e1f12ddd0cf673363783969df25df14269724108
nandaye <18281908275@163.com>
2016-02-16 13:32:38 +0800

    Initial commit to test git Add hello World! 模板测试

--- src/com/nanye/Java8/HelloWorld.java
+++ src/com/nanye/Java8/HelloWorld.java
@@ -8,5 +8,10 @@ public class HelloWorld {
         String my_name = "nandaye";
         System.out.println("Yes ,this is a program to print hello world!");
         System.out.println("my_name = " + my_name);
+        int i=50;
+        for (int i1 = 0; i1 < i; i1++) {
+            System.out.println("this is "+i1);
+        }
+
     }
 }
